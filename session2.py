270b7d39d0d60ada33fcf981a5d9fe69452f5a0f
msa <msai.sadehi@gmail.com>
2023-06-11 14:47:55 +0330

    second session

--- session2.py
+++ session2.py
@@ -1,1 +1,36 @@
-print("Hello World")
+# print("Hello World")
+# print("2+3")
+# print(2 + 3)
+
+# score = 10
+# print("new score is:", score + 1)
+# print("new score is:", "score + 1")
+# این اولین برنامه ما است
+# print("he said \"something\"")
+# message = "Hello every body. \nwelcome to python class. \nwe are going to learn everythings about python. \npython is the king of programming languages."
+# print(message)
+
+
+# message = """Hello every body. 
+# welcome to python class. 
+# we are going to learn everythings about python. 
+# python is the king of programming languages."""
+# print(message)
+
+
+x = 10
+y = 25
+
+
+z = x + y
+
+print(z)
+print("z =",z)
+
+# دو متغیر تعریف کن 
+# در اولی عدد بیست 
+# و در دومی عدد سی را قرار بده
+# جاصل جمع، ضرب ، تقسیم و تفریق دو عدد را محاسبه و نمایش بده
+# +   *   -   /
+
+score_1 = 1
