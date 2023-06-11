355931346f37cfe28ca74fdd5adda7206653fc18
msa <msai.sadehi@gmail.com>
2023-06-11 14:59:18 +0330

    second session

--- session2.py
+++ session2.py
@@ -18,14 +18,14 @@
 # print(message)
 
 
-x = 10
-y = 25
+# x = 10
+# y = 25
 
 
-z = x + y
+# z = x + y
 
-print(z)
-print("z =",z)
+# print(z)
+# print("z =",z)
 
 # دو متغیر تعریف کن 
 # در اولی عدد بیست 
@@ -33,4 +33,37 @@ print("z =",z)
 # جاصل جمع، ضرب ، تقسیم و تفریق دو عدد را محاسبه و نمایش بده
 # +   *   -   /
 
-score_1 = 1
+# score_1 = 1
+
+
+# score = 0
+
+# score = score + 1
+
+# print(score)
+
+# x = 4
+# y = 2
+# z = x % y
+# print(z)
+
+# x = 5
+# y = 2
+# z = x % y
+# print(z)
+
+# print(5 % 2)
+
+# print(4/2)
+# print(5/2)
+# print(5//2)
+
+
+score_1 = 90
+score_2 = 80
+score_3 = 95
+average = (score_1 + score_2 + score_3) /3
+print("average is:", average)
+
+
+# input ? next session
