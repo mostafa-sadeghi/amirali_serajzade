cd644218b6770d023fd69f1304f7fc1fc464023e
msa <msai.sadehi@gmail.com>
2023-06-25 14:58:58 +0330

    sadeghi

--- session3.py
+++ session3.py
@@ -1,24 +1,36 @@
-name = input("what's your name? ")
-print("hello", name)
+# name = input("what's your name? ")
+# print("hello", name)
 
-age = int(input("Enter your age? "))
-print("age =", age)
-if age > 8:
-    print("you can play")
-else:
-    print("you can't play")
+# age = int(input("Enter your age? "))
+# print("age =", age)
+# if age > 8:
+#     print("you can play")
+# else:
+#     print("you can't play")
 
-number1 = int(input('enter a number: '))
-number2 = int(input('enter a number: '))
+# number1 = int(input('enter a number: '))
+# number2 = int(input('enter a number: '))
 
-print("number1 + number2 =", number1 + number2)
+# print("number1 + number2 =", number1 + number2)
 
-number1 = float(input('enter a number: '))
-number2 = float(input('enter a number: '))
+# number1 = float(input('enter a number: '))
+# number2 = float(input('enter a number: '))
 
-print("number1 + number2 =", number1 + number2)
+# print("number1 + number2 =", number1 + number2)
 
 # TODO   تمرین دو عدد از ورودی دریافت کنی و حاصل جمع، تفریق، تقسیم و ضرب دو عدد را نمایش دهد
+
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
+
+# print("number1 + number2 =", number1 + number2)
+# print("number1 - number2 =", number1 - number2)
+# print("number1 * number2 =", number1 * number2)
+# print("number1 / number2 =", number1 / number2)
+
+
+
+
 # TODO   برنامه ای بنویس که نام و نمرات سه درس یک دانش آموز را از ورودی دریافت نماید و
 # معدل او را محاسبه کند
 # معدل یعنی حمع نمرات تقسیم بر تعداشان
@@ -26,3 +38,13 @@ print("number1 + number2 =", number1 + number2)
 # A
 # را پرینت نماید
 # در عیر اینصورت فقط معدل او را پرینت کنید
+
+# name = input("Enter your name:> ")
+# score_1 = float(input("Enter score one:> "))
+# score_2 = float(input("Enter score two:> "))
+# score_3 = float(input("Enter score three:> "))
+# ave = (score_1 + score_2 + score_3) / 3
+# if ave >= 19:
+#     print("A")
+# else:
+#     print("average is:", ave)
---
+++ session4.py
@@ -0,0 +1,66 @@
+# a = 4
+# b = 5
+# c = 3
+
+# if a < b:
+#     print("a is less than b")
+#     print("blalalla")
+
+# print("outside")
+
+
+# if a > b and a > c:
+#     print("a is the greatest")
+# elif b > c and b > a:
+#     print("b is the greatst")
+# elif c > a and c > b:
+#     print("c is the greatest")
+
+
+# user_name = input("Enter the user name:> ")
+# password = input("Enter the password:> ")
+
+# if user_name == "sara" and password == "123":
+#     print("user is valid")
+# else:
+#     print("user is not valid")
+
+
+# print(3>4)
+# print(3<4)
+
+# score = 20
+
+# if score < 20:
+#     print("Level one")
+
+# elif score >= 20:
+#     print("Next Level")
+
+
+# print(3 != 4)
+
+
+# a = 3
+# b = 4
+
+# if a == b:
+#     print("a and b are equal")
+
+# if a != b:
+    # print("a and b are not equal")
+
+
+ave = 18
+
+if ave >= 19:
+    print("A")
+
+elif ave >=18:
+    print("B")
+elif ave >= 17:
+    print("C")
+elif ave >= 16:
+    print("D")
+else:
+    print("E")
